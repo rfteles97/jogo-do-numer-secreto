ce96da40499df3adbb344ded3f49df4475607a06
rafael teles <rafteles2016@hotmail.com>
2025-09-14 16:56:12 -0300

    Revert "mudanças no titulo e gitignore"
    This reverts commit 86a5687780d26358a044354bf473ddf1d8ecc4b6.

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1','Jogo do Número Secreto');
+    exibirTextoNaTela('p','Escolha um número entre 1 e 10');
 
 }
 exibirMensagemInicial();
