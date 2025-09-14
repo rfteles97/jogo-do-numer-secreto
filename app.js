dc42ac8e8996ef7f4a572764399de06389c31732
rafael teles <rafteles2016@hotmail.com>
2025-09-14 16:06:03 -0300

    teste54

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ function exibirTextoNaTela(tag, texto){
 }
 
 function exibirMensagemInicial() {
-    exibirTextoNaTela('h1','Jogo do Número Secreto');
-    exibirTextoNaTela('p','Escolha um número entre 1 e 10');
+
+
 }
 exibirMensagemInicial();
 
